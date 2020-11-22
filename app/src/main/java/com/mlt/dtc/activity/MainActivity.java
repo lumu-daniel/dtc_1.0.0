88da821ecf5bb591f5b296cc3dd874edfee45efd
azhar <azhar.cv90@gmail.com>
2020-11-22 10:44:34 +0400

    second commit

--- app/src/main/java/com/mlt/dtc/activity/MainActivity.java
+++ app/src/main/java/com/mlt/dtc/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.mlt.dtc;
+package com.mlt.dtc.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.mlt.dtc.R;
 
 public class MainActivity extends AppCompatActivity {
 
