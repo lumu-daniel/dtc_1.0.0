51702666eaf95900301d68b45aadb705976e6b35
azhar <azhar.cv90@gmail.com>
2020-11-22 12:21:02 +0400

    add id

--- app/src/main/java/com/mlt/dtc/activity/MainActivity.java
+++ app/src/main/java/com/mlt/dtc/activity/MainActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
+import com.github.infinitebanner.InfiniteBannerView;
 import com.mlt.dtc.R;
 import com.mlt.dtc.common.SystemUIService;
 
 public class MainActivity extends AppCompatActivity {
+    public InfiniteBannerView infiniteBannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +19,13 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+        findviewbyid();
+
         HideNavigationBar();
+    }
+
+    private void findviewbyid() {
+        infiniteBannerView = findViewById(R.id.pager);
     }
 
 
--- app/src/main/java/com/mlt/dtc/common/Common.java
+++ app/src/main/java/com/mlt/dtc/common/Common.java
@@ -1,6 +1,9 @@
 package com.mlt.dtc.common;
 
+import android.os.Environment;
+
 public
 class Common {
 
+    public static  String multimediaPath = "sdcard/"+ Environment.DIRECTORY_DCIM +"/Multimedia/";
 }
